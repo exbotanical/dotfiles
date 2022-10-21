f3222f3723a2e676ddc9534c9d6e83ff1a3618ad
Matthew Zito <exbotanical@protonmail.com>
2022-10-21 11:31:25 -0500

    chore: update keybindings, actions

--- bash/.config/bash/alias.bash
+++ bash/.config/bash/alias.bash
@@ -27,28 +27,31 @@ alias ......="cd ../../../../.."
 ### End Navigation ### }}}
 
 ### Git {{{
+# I prefer defining my Git aliases in bash over gitconfig
 alias gpull='git pull'
 alias gpush='git push'
 alias ga='git add'
+# Git add all
 alias gaa='git add .'
 alias gs='git status'
 alias gunstage='git reset'
 alias gstash='git stash'
-# prettify log
+# Prettify log
 alias gl='git log --pretty=format:"%C(yellow)%h%Cred%d %Creset%s%Cblue [%cn]" --decorate'
-# fetch all remotes
+# Fetch all remotes
 alias gfetch='git fetch --all'
-# fetch all remotes and track ea.
+# Fetch all remotes and track each
 alias gfetchtrack='git branch -r | grep -v "\->" | while read remote; do git branch --track "${remote#origin/}" "$remote"; done'
-# stage all changes and take them to a new branch
+# Stage all changes and take them to a new branch
 alias gswitch='git add . && git switch -c'
-# signed commit
-alias gc='git commit -S'
+alias gc='git commit'
+# Amend commit
+alias gca='git commit --amend'
 alias gbg="git bisect good"
 alias gbb="git bisect bad"
-# list all branches from least to most recent
+# List all branches from least to most recent
 alias gh="git for-each-ref --sort=-committerdate refs/heads/ --format='%(refname:short)' | tac"
-# worktrees :)
+# Worktrees :)
 alias gw='git worktree add worktree'
 alias gwl='git worktree list'
 alias gwrm='git worktree remove worktree && git worktree prune'
