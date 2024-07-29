3b1fc12797f709e598195082e6cc4849e734e4fe
Matthew Zito <mtzito@amazon.com>
2024-07-29 10:52:31 -0700

    chore(macos): add missing macos configs

--- bash/.config/bash/apps/brew/env.bash
+++ bash/.config/bash/apps/brew/env.bash
@@ -1,2 +1,6 @@
-init::append_path /opt/homebrew/bin
+# Escape hatch for osx - system adds homebrew to the PATH prior to bash init.
+# Forcibly remove it and prepend it to the PATH.
+# Otherwise, the shell env will prefer system defaults over homebrew, which we want.
+export PATH="/opt/homebrew/bin:$(echo $PATH | sed 's/\:\/opt\/homebrew\/bin//g')"
+
 init::export BASH_COMPLETION_COMPAT_DIR "$(brew --prefix)/etc/bash_completion.d"
