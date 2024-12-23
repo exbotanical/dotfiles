044616eaf8eec6809c1a92868cb595c1fe359753
Matthew Zito <exbotanical@protonmail.com>
2024-12-23 08:16:43 -0800

    chore(bash): update default bt headphones mac addr

--- bash/.config/bash/apps/bluetoothctl/cmd.bash
+++ bash/.config/bash/apps/bluetoothctl/cmd.bash
@@ -1,7 +1,15 @@
+DEFAULT_HEADPHONES_MACADDR='28:F0:33:D0:61:14'
+
 # blue_conn connects a bluetooth device by its MAC address
 blue_conn () {
-  local default='28:F0:33:D0:61:14'
+  local macaddr="${1:-$DEFAULT_HEADPHONES_MACADDR}"
 
   # must have edited config rules
-  bluetoothctl connect "${1:-$default}"
+  bluetoothctl connect $macaddr
+}
+
+blue_reset () {
+  local macaddr="${1:-$DEFAULT_HEADPHONES_MACADDR}"
+
+  bluetoothctl disconnect $macaddr && blue_conn $macaddr
 }
