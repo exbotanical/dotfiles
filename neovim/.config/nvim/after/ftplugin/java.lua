291962141bf939fd496f6f434c1fea231627bbe9
Matthew Zito <exbotanical@protonmail.com>
2025-02-26 21:36:30 -0800

    chore: remove unused neovim stuff

--- neovim/.config/nvim/after/ftplugin/java.lua
+++
@@ -1,3 +0,0 @@
-vim.o.expandtab = false      -- Don't convert tabs to spaces
-vim.bo.tabstop = 4           -- Indent using n spaces
-vim.bo.shiftwidth = 4        -- When shifting, indent using n spaces
--- neovim/.config/nvim/after/plugin/defaults.lua
+++
@@ -1,3 +0,0 @@
-local opt = vim.opt
-
-opt.winbar = "%{%v:lua.require'config.winbar'.get_winbar()%}"
