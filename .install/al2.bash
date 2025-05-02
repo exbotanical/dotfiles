bd77a75fe8c9cfce5c60ffc8a9e9bdf6892a1ef8
Matthew Zito <exbotanical@protonmail.com>
2025-05-02 14:56:39 -0700

    fix: copy to /home/goldmund and not cwd for al2 postinstall hook

--- .install/al2.bash
+++ .install/al2.bash
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Prevents `/usr/bin/cat $HOME/.dir_colors: Is a directory`
-mv $HOME/.dir_colors/* . && rm -rf $HOME/.dir_colors
+mv $HOME/.dir_colors/* $HOME && rm -rf $HOME/.dir_colors
